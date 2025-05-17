9b11a85f138dd2c6434ff1ac82501c08fe684f86
Kate Novak <kje@cochleoid.com>
2025-05-17 16:03:06 -0400

    a list so static...

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ type ChuckMessage struct {
 	Value string `json:"value"`
 }
 
-func getColorMsg(message string) {
+func printColorMsg(message string) {
 	r, g, b := 255, 215, 0 // gold color
 	fmt.Printf("\033[38;2;%d;%d;%dm%s\033[0m\n", r, g, b, message)
 }
@@ -21,67 +21,62 @@ func getColorMsg(message string) {
 func main() {
 	// Check if command-line arguments are provided
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: chuck-norris-says [random|category|list-categories] [search_category]")
+		fmt.Println("Usage: chuck-norris-says [random|category|list-categories|help] [search_category]")
 		return
 	}
 
 	command := os.Args[1]
 	if command == "help" {
-		getColorMsg("Chuck Norris needs no help, pussy")
-		return
-	} else if command != "random" && command != "category"  && command != "list-categories" {
-		msg := fmt.Sprintf("Chuck Norris doesn't know this command: %s", command)
-		getColorMsg(msg)
-		return
-	} else if command == "list-categories" {
-		categories := []string{
-			"animal",
-			"career",
-			"celebrity",
-			"dev",
-			"explicit",
-			"fashion",
-			"food",
-			"history",
-			"money",
-			"movie",
-			"music",
-			"political",
-			"religion",
-			"science",
-			"sport",
-			"travel",
+		printColorMsg("Chuck Norris needs no help, pussy.")
+	} else if command == "random" || command == "category" {
+		var category string = ""
+		if command == "category" && len(os.Args) < 3 {
+			printColorMsg("Chuck Norris says you're missing a category.\nRun chuck-norris-says category [search_category]")
+		} else if len(os.Args) > 2 {
+			category = os.Args[2]
 		}
-		msg := fmt.Sprintf("Chuck's cats: \n %s\n", strings.Join(categories, ",\n "))
-		getColorMsg(msg)
-		return
-	}
 
-	var category string = ""
-	if command == "category" && len(os.Args) < 3 {
-		getColorMsg("Chuck Norris says you're missing a category.\nRun chuck-norris-says category [search_category]")
-	} else if len(os.Args) > 2 {
-		category = os.Args[2]
-	}
+		var url string = "https://api.chucknorris.io/jokes/random"
+		if command == "category" {
+			url = fmt.Sprintf(url+"?category=%s", category)
+		}
 
-	var url string = "https://api.chucknorris.io/jokes/random"
-	if command == "category" {
-		url = fmt.Sprintf(url + "?category=%s", category)
-	}
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
 
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Read error: ", err)
+			return
+		}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Read error: ", err)
-		return
-	}
+		var chuck_message ChuckMessage
+		json.Unmarshal(body, &chuck_message)
+		printColorMsg(chuck_message.Value)
+	} else if command == "list-categories" {
+		var url string = "https://api.chucknorris.io/jokes/categories"
 
-	var chuck_message ChuckMessage
-	json.Unmarshal(body, &chuck_message)
-	getColorMsg(chuck_message.Value)
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Read error: ", err)
+			return
+		}
+
+		var cats []string
+		json.Unmarshal(body, &cats)
+		msg := fmt.Sprintf("Chuck's cats: \n %s\n", strings.Join(cats, ",\n "))
+		printColorMsg(msg)
+	} else {
+		msg := fmt.Sprintf("Chuck Norris doesn't know this command: %s", command)
+		printColorMsg(msg)
+	}
 }
